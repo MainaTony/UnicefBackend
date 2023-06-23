08fa21eaeb5e1314549121a8031aa244043a24c0
Tony Maina <tonymaina36@gmail.com>
2023-06-23 10:52:45 +0300

    Added organisation service and service implementation

---
+++ src/main/java/com/pinnoserv/portal/service/OrganisationService.java
@@ -0,0 +1,8 @@
+package com.pinnoserv.portal.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface OrganisationService {
+    public String getOrganisation();
+}
---
+++ src/main/java/com/pinnoserv/portal/service/OrganisationServiceImpl.java
@@ -0,0 +1,8 @@
+package com.pinnoserv.portal.service;
+
+public class OrganisationServiceImpl implements OrganisationService{
+    @Override
+    public String getOrganisation() {
+        return null;
+    }
+}
