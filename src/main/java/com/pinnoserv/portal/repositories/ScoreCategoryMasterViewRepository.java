1eaebf6de10bd8b8d42a87746273b3745b63f5e9
Tony Maina <tonymaina36@gmail.com>
2023-07-18 13:17:23 +0300

    Added score Category matser

--- src/main/java/com/pinnoserv/portal/repositories/ScoreCategoryMasterViewRepository.java
+++ src/main/java/com/pinnoserv/portal/repositories/ScoreCategoryMasterViewRepository.java
@@ -1,20 +1,20 @@
-//package com.pinnoserv.portal.repositories;
-//
-//import com.pinnoserv.portal.entity.ScoreCategoryMaster;
-////import com.pinnoserv.portal.view.ScoreCategoryMasterView;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//import java.math.BigInteger;
-//import java.util.List;
-//import java.util.Optional;
-//
-///**
-// *
-// * @author mwendwakelvin
-// */
-//@Repository
-//public interface ScoreCategoryMasterViewRepository extends JpaRepository<ScoreCategoryMaster, Integer> {
-//    Optional<ScoreCategoryMaster> findById(Integer id);
-//
-//    List<ScoreCategoryMaster> findAllByProductIdFk(Integer productId);
-//}
+package com.pinnoserv.portal.repositories;
+
+import com.pinnoserv.portal.entity.ScoreCategoryMaster;
+//import com.pinnoserv.portal.view.ScoreCategoryMasterView;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.math.BigInteger;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ *
+ * @author mwendwakelvin
+ */
+@Repository
+public interface ScoreCategoryMasterViewRepository extends JpaRepository<ScoreCategoryMaster, Integer> {
+    Optional<ScoreCategoryMaster> findById(Integer id);
+
+    List<ScoreCategoryMaster> findAllByProductIdFk(Integer productId);
+}
